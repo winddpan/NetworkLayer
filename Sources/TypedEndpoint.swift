f0a632d35f4bea63b0f16840565211bcdaf68511
winddpan <winddpan@126.com>
2023-07-20 23:18:10 +0800

    TypedEndpoint

--- Sources/TypedEndpoint.swift
+++ Sources/TypedEndpoint.swift
@@ -1,15 +1,34 @@
 import Foundation
 
-public protocol TypedEndpoint: Endpoint {
-    associatedtype ModelType: Decodable
-    var modelType: (ModelType.Type, String?) { get }
+public struct TypedEndpoint<ModelType: Decodable>: Endpoint {
+    public let baseURL: String
+    public let path: String
+    public let method: HTTPMethod
+    public let header: [String: String]?
+    public let body: BodyConvertible?
+    public let modelKeyPath: String?
+
+    public init(baseURL: String,
+                path: String,
+                method: HTTPMethod,
+                header: [String: String]? = nil,
+                body: BodyConvertible? = nil,
+                modelType: ModelType.Type,
+                modelKeyPath: String? = nil) {
+        self.baseURL = baseURL
+        self.path = path
+        self.method = method
+        self.header = header
+        self.body = body
+        self.modelKeyPath = modelKeyPath
+    }
 }
 
-public extension NetworkProvider where T: TypedEndpoint {
+public extension NetworkProvider {
     @discardableResult
-    func requestModel(_ endpoint: T) async throws -> T.ModelType {
+    func requestModel<M>(_ endpoint: T) async throws -> M where T == TypedEndpoint<M> {
         let response = try await request(endpoint)
-        let model = try response.map(T.ModelType.self, atKeyPath: endpoint.modelType.1)
+        let model = try response.map(M.self, atKeyPath: endpoint.modelKeyPath)
         return model
     }
 }
